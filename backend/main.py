fe9496fe14afb44a6e59e037400648cbbd23432e
Harsha <152892377+harsha-9605@users.noreply.github.com>
2026-04-23 17:10:35 +0530

    Change MongoDB connection string
    Updated MongoDB connection string with credentials.

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 # --- MONGODB CONNECTION ---
 import certifi
 
-MONGO_URL = "mongodb string"
+MONGO_URL = "mongodb+srv://Harsha_96:Sanji_9605@cluster0.bfcuzb2.mongodb.net/?appName=Cluster0"
 
 client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 db = client.anime_db  # Database name
